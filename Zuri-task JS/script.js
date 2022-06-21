0bda06ac5b2eddff6a39ec452a4796bb3ad4f80d
Bolaji Jimoh Adigun <bolajijimoh8@gmail.com>
2022-06-21 13:01:41 +0100

    First JavaScript Task on Zuri Academy
    To write a code to print the output of name, height and country on the screen

---
+++ Zuri-task JS/script.js
@@ -0,0 +1,7 @@
+var name, height, country ;
+name = "Bolaji";
+height = 67;
+country= 'Nigeria';
+document.getElementById("zuri1").innerHTML = name;
+document.getElementById("zuri2").innerHTML = height;
+document.getElementById("zuri3").innerHTML = country;
